7d49b31bfdbae5175930e2a3e5aded567f5312d0
Andrej Dundovic <andrej.dundovic@desy.de>
2020-04-22 14:06:49 +0200

    Some optimizations in HEALPixBits

--- src/HEALPixBits.cpp
+++ src/HEALPixBits.cpp
@@ -29,6 +29,15 @@ inline long nside2order(unsigned int nside) {
 	return ((nside)&(nside-1)) ? -1 : log2(nside);
 }
 
+unsigned int ang2pix_ring(unsigned int nside, const QDirection &thetaphi) {
+	double theta = static_cast<double>(thetaphi[0]);
+	double phi = static_cast<double>(thetaphi[1]);
+
+	return ((theta<0.01) || (theta > 3.14159-0.01)) ?
+        	loc2pix(nside, cos(theta),phi,sin(theta),true) :
+	        loc2pix(nside, cos(theta),phi,0.,false);
+}
+
 QDirection pix2ang_ring(unsigned int nside, unsigned int ipix) {
 	QDirection thetaphi = {0, 0};
 
@@ -47,8 +56,8 @@ QDirection pix2ang_ring(unsigned int nside, unsigned int ipix) {
 	// ! North Polar cap 
 	if( ipix1 <= ncap ) {
 		const double hip = ipix1/2.;
-		const double fihip = std::floor(hip);
-		const unsigned int iring = (int)std::floor( std::sqrt(hip - std::sqrt(fihip) ) ) + 1;// ! counted from North pole
+		const unsigned int fihip = std::floor(hip);
+		const unsigned int iring = (unsigned)std::floor( std::sqrt(hip - std::sqrt(fihip) ) ) + 1;// ! counted from North pole
 		const unsigned int iphi  = ipix1 - 2*iring*((int)iring - 1);
 
 		thetaphi[0] = std::acos(1. - iring*iring / fact2 );
@@ -60,12 +69,14 @@ QDirection pix2ang_ring(unsigned int nside, unsigned int ipix) {
 	// ! Equatorial region
 	if( ipix1 <= nl2*(5*nside+1) ) {
 		const unsigned ip = ipix1 - ncap - 1;
-		const unsigned int iring = (int)std::floor( ip / nl4 ) + nside;// ! counted from North pole
-		const unsigned int iphi  = (int)std::fmod(ip,nl4) + 1;
+		const unsigned int iring = (unsigned)std::floor( ip / nl4 ) + nside;// ! counted from North pole
+		const unsigned int iphi  = (unsigned)std::fmod(ip,nl4) + 1;
 
 		const double fodd  = 0.5 * (1 + std::fmod((double)(iring+nside),2));//  ! 1 if iring+nside is odd, 1/2 otherwise
+
 		thetaphi[0] = std::acos( ((int)nl2 - (int)iring) / fact1 );
 		thetaphi[1] = (1.*iphi - fodd) * pi/(2.*nside);
+
 		return thetaphi;
 	}
 
@@ -73,25 +84,15 @@ QDirection pix2ang_ring(unsigned int nside, unsigned int ipix) {
 	{
 		const unsigned ip = npix - ipix1 + 1;
 		const double hip = ip/2.;
-		/* bug corrige floor instead of 1.* */
-		const double fihip = std::floor(hip);
+		const unsigned int fihip = std::floor(hip);
 		const unsigned int iring = (int)std::floor( std::sqrt(hip - std::sqrt(fihip))) + 1;//     ! counted from South pole
-		const unsigned iphi  = (int)(4.*iring + 1 - ((int)ip - 2.*(int)iring*((int)iring-1)));
+		const unsigned iphi = (int)(4.*iring + 1 - ((int)ip - 2.*(int)iring*((int)iring-1)));
 
 		thetaphi[0] = std::acos(-1. + (int)iring*iring / fact2 );
 		thetaphi[1] = (1.*iphi - 0.5) * pi/(2.*iring);
+		
 		return thetaphi;
 	}
-}
-
-
-unsigned int ang2pix_ring(unsigned int nside, const QDirection &thetaphi) {
-	double theta = static_cast<double>(thetaphi[0]);
-	double phi = static_cast<double>(thetaphi[1]);
-
-	return ((theta<0.01) || (theta > 3.14159-0.01)) ?
-        	loc2pix(nside, cos(theta),phi,sin(theta),true) :
-	        loc2pix(nside, cos(theta),phi,0.,false);
 }
 
 unsigned int loc2pix(unsigned int nside, double z, double phi, double sth, bool have_sth) {
@@ -109,15 +110,15 @@ unsigned int loc2pix(unsigned int nside, double z, double phi, double sth, bool 
 		const unsigned int nl4 = 4*nside;
 		const double temp1 = nside*(0.5+tt);
 		const double temp2 = nside*z*0.75;
-		long jp = long(temp1-temp2); // index of  ascending edge line
-		long jm = long(temp1+temp2); // index of descending edge line
+		const long jp = std::floor(temp1-temp2); // index of  ascending edge line
+		const long jm = std::floor(temp1+temp2); // index of descending edge line
 
 		// ring number counted from z=2/3
-		long ir = nside + 1 + jp - jm; // in {1,2n+1}
-		long kshift = 1-(ir&1); // kshift=1 if ir even, 0 otherwise
+		const long ir = nside + 1 + jp - jm; // in {1,2n+1}
+		const long kshift = 1-(ir&1); // kshift=1 if ir even, 0 otherwise
 
-		long t1 = jp+jm-nside+kshift+1+nl4+nl4;
-		long ip = (order>0) ?
+		const long t1 = jp+jm-nside+kshift+1+nl4+nl4;
+		const long ip = (order>0) ?
 			(t1>>1)&(nl4-1) : ((t1>>1)%nl4); // in {0,4n-1}
 
 		return ncap + (ir-1)*nl4 + ip;
@@ -125,18 +126,16 @@ unsigned int loc2pix(unsigned int nside, double z, double phi, double sth, bool 
 	
 	// North & South polar caps
 	{
-		double tp = tt - std::floor(tt);
-		double tmp = ((za<0.99)||(!have_sth)) ?
+		const double tp = tt - std::floor(tt);
+		const double tmp = ((za<0.99)||(!have_sth)) ?
 			nside*std::sqrt(3*(1-za)) :
 			nside*sth/std::sqrt((1.+za)/3.);
 
-		long jp = long(tp*tmp); // increasing edge line index
-		long jm = long((1.0-tp)*tmp); // decreasing edge line index
+		const long jp = std::floor(tp*tmp); // increasing edge line index
+		const long jm = std::floor((1.0-tp)*tmp); // decreasing edge line index
 
-		long ir = jp+jm+1; // ring number counted from the closest pole
-		long ip = long(tt*ir); // in {0,4*ir-1}
-		//planck_assert((ip>=0)&&(ip<4*ir),"must not happen");
-		//ip %= 4*ir;
+		const long ir = jp+jm+1; // ring number counted from the closest pole
+		const long ip = std::floor((double)ir*tt); // in {0,4*ir-1}
 
 		return (z>0) ? 2*ir*(ir-1) + ip : nside2npix(nside) - 2*ir*(ir+1) + ip;
 	}
