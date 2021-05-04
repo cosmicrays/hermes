54f855473b066541b7251dfb6d7a17eb892a73ee
Andrej Dundovic <andrej@dundovic.com.hr>
2021-05-04 17:57:10 +0200

    python __version__ added

--- python/Common.cpp
+++ python/Common.cpp
@@ -1,9 +1,11 @@
 #include "hermes/Common.h"
+#include "hermes/Version.h"
+#include "hermes/HEALPixBits.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "hermes/HEALPixBits.h"
+#include <string>
 
 namespace py = pybind11;
 
@@ -22,6 +24,8 @@ void init_common(py::module &m) {
 	m.def("nside2npix", &nside2npix);
 	m.def("nside2order", &nside2order);
 	m.def("loc2pix", &loc2pix);
+
+    m.attr("__version__") = std::string(g_GIT_DESC);
 }
 
 }  // namespace hermes
