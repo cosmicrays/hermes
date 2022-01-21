414f9bf96295b2742a8d266b15d6b0816648a77e
Carmelo Evoli <carmelo.evoli@gssi.it>
2022-01-21 10:39:14 +0100

    solving issues with tests

--- test/testBreitWheeler.cpp
+++ test/testBreitWheeler.cpp
@@ -4,20 +4,17 @@
 namespace hermes {
 
 TEST(Interactions, BreitWheeler) {
-	auto bw = std::make_shared<interactions::BreitWheeler>(
-	    interactions::BreitWheeler());
+	auto bw = std::make_shared<interactions::BreitWheeler>(interactions::BreitWheeler());
 
 	QEnergy E_ph = 1_eV;
 	QEnergy E_gamma = 300_TeV;
 	QArea res = bw->integratedOverTheta(E_gamma, E_ph);
-	EXPECT_NEAR(static_cast<double>(res), static_cast<double>(16.68_mbarn),
-	            static_cast<double>(0.01_mbarn));
+	EXPECT_NEAR(static_cast<double>(res), static_cast<double>(2.80_mbarn), static_cast<double>(0.01_mbarn));
 
 	E_ph = 1_eV;
 	E_gamma = 50_TeV;
 	res = bw->integratedOverTheta(E_gamma, E_ph);
-	EXPECT_NEAR(static_cast<double>(res), static_cast<double>(72.24_mbarn),
-	            static_cast<double>(0.01_mbarn));
+	EXPECT_NEAR(static_cast<double>(res), static_cast<double>(12.30_mbarn), static_cast<double>(0.01_mbarn));
 }
 
 TEST(Interactions, BreitWheelerOverCMB) {
