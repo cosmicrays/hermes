57cfe261653ae9054db4d18b968ccb235fcb9772
jveh <johannes.veh@fau.de>
2022-05-05 12:57:04 +0200

    cmake can find fftw and cfitsio now from ENV: FFTW FFTW_BASE and CFITSIO CFITSIO_BASE

--- cmake/FindFFTW3F.cmake
+++ cmake/FindFFTW3F.cmake
@@ -3,8 +3,18 @@
 # FFTW3F_INCLUDE_DIR = fftw3.h
 # FFTW3F_LIBRARIES = libfftw3f.a .so
 
-find_path(FFTW3F_INCLUDE_DIR fftw3.h)
-find_library(FFTW3F_LIBRARIES fftw3f)
+
+find_path(FFTW3F_INCLUDE_DIR fftw3.h
+        PATH_SUFFIXES include inc
+        PATHS $ENV{FFTW_BASE} $ENV{FFTW}
+        )
+message(STATUS "FFTW3F header => ${FFTW3F_INCLUDE_DIR}")
+
+find_library(FFTW3F_LIBRARIES fftw3f
+        PATH_SUFFIXES lib
+        PATHS  $ENV{FFTW_BASE} $ENV{FFTW}
+        )
+message(STATUS "FFTW3F libs => ${FFTW3F_LIBRARIES}")
 
 set(FFTW3F_FOUND FALSE)
 if(FFTW3F_INCLUDE_DIR AND FFTW3F_LIBRARIES)
