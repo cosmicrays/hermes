cf0d07873cc0e653e05aa0e162d2e1af4a0cdbae
Carmelo Evoli <carmelo.evoli@gssi.it>
2026-06-05 16:10:45 +0200

    fix testRingModel

--- test/testRingModel.cpp
+++ test/testRingModel.cpp
@@ -21,12 +21,13 @@ TEST_F(RingModel, RingBoundaries) {
 		result.push_back(ring->getBoundaries());
 	}
 
-	std::array<QLength, 12> b = {0_kpc, 2_kpc, 3_kpc,  4_kpc,  5_kpc,  6_kpc,
-	                             7_kpc, 9_kpc, 12_kpc, 15_kpc, 18_kpc, 35_kpc};
+	std::array<QLength, 12> expectedOuterEdges = {
+	    2.3_kpc, 3.3_kpc, 4.3_kpc, 5.3_kpc,  6.3_kpc,  7.3_kpc,
+	    9.3_kpc, 12.3_kpc, 15.3_kpc, 18.3_kpc, 35.3_kpc, 36_kpc};
 
-	EXPECT_EQ(static_cast<double>(result[0].second), static_cast<double>(b[1]));
-	EXPECT_EQ(static_cast<double>(result[1].second), static_cast<double>(b[2]));
-	EXPECT_EQ(static_cast<double>(result[11].first), static_cast<double>(b[11]));
+	EXPECT_NEAR(static_cast<double>(result[0].second / kpc), static_cast<double>(expectedOuterEdges[0] / kpc), 1e-12);
+	EXPECT_NEAR(static_cast<double>(result[1].second / kpc), static_cast<double>(expectedOuterEdges[1] / kpc), 1e-12);
+	EXPECT_NEAR(static_cast<double>(result[11].second / kpc), static_cast<double>(expectedOuterEdges[11] / kpc), 1e-12);
 
 	EXPECT_TRUE(ringModel_HI[3]->isInside(Vector3QLength(0, 4.5_kpc, 3.0_kpc)));
 	EXPECT_FALSE(ringModel_HI[3]->isInside(Vector3QLength(1.5_kpc, 2.5_kpc, 3.0_kpc)));
