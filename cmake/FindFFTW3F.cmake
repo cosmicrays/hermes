65c7a46372b3011c1ab00efe1786db10e1b217d3
Carmelo Evoli <carmelo.evoli@gssi.it>
2026-08-25 16:18:01 +0200

    build: provide imported dependency targets

--- cmake/FindFFTW3F.cmake
+++ cmake/FindFFTW3F.cmake
@@ -8,23 +8,19 @@ find_path(FFTW3F_INCLUDE_DIR fftw3.h
         PATH_SUFFIXES include inc
         PATHS $ENV{FFTW_BASE} $ENV{FFTW}
         )
-message(STATUS "FFTW3F header => ${FFTW3F_INCLUDE_DIR}")
-
 find_library(FFTW3F_LIBRARIES fftw3f
         PATH_SUFFIXES lib
         PATHS  $ENV{FFTW_BASE} $ENV{FFTW}
         )
-message(STATUS "FFTW3F libs => ${FFTW3F_LIBRARIES}")
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FFTW3F
+    REQUIRED_VARS FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIR)
 
-set(FFTW3F_FOUND FALSE)
-if(FFTW3F_INCLUDE_DIR AND FFTW3F_LIBRARIES)
-    set(FFTW3F_FOUND TRUE)
-    MESSAGE(STATUS "FFTW3 with single precision (FFTW3F): Found!")
-else()
-    MESSAGE(STATUS "FFTW3 with single precision (FFTW3F): NOT Found!")
+if(FFTW3F_FOUND AND NOT TARGET FFTW3F::FFTW3F)
+    add_library(FFTW3F::FFTW3F UNKNOWN IMPORTED)
+    set_target_properties(FFTW3F::FFTW3F PROPERTIES
+        IMPORTED_LOCATION "${FFTW3F_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${FFTW3F_INCLUDE_DIR}")
 endif()
-
-MESSAGE(STATUS "  Include:     ${FFTW3F_INCLUDE_DIR}")
-MESSAGE(STATUS "  Library:     ${FFTW3F_LIBRARIES}")
 
 mark_as_advanced(FFTW3F_INCLUDE_DIR FFTW3F_LIBRARIES FFTW3F_FOUND)
